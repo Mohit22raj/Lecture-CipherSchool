b4192a29f7f22bbba33b8611a9c96188aba78077
Mohit22raj <118985478+Mohit22raj@users.noreply.github.com>
2023-07-04 19:44:03 +0530

    Update Lecture 7.java

--- Lecture 7.java
+++ Lecture 7.java
@@ -46,9 +46,9 @@ public class lec_7 {
     public static void main(String args[]) {
         Student s1 = new Student();
         Student s2 = new Student();
-        s1.name = "Suryansh";
+        s1.name = "Mohit";
         s1.marks = 30;
-        s2.name = "Sushant";
+        s2.name = "Raj";
         s2.marks = 90;
         System.out.println(s1.is_pass());
         System.out.println(s2.is_pass());
